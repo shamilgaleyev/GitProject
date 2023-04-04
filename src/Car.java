5e51237c3d0d824c3655007da2adcb1ef9c72f0c
shamilgaleyev <galeyevshamil@gmail.com>
2023-04-03 22:05:54 -0400

    first commit

---
+++ src/Car.java
@@ -0,0 +1,5 @@
+public class Car {
+    String mark;
+    String model;
+    String color;
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.mark="toyota";
+        car.model="Rav4";
+        car.color="white";
+    }
+}
